26bac29f5663e56e6cbbeae4b20c1f653411d289
Ole <ole@Oles-MacBook-Pro.local>
2018-09-26 13:58:52 +0200

    Refactor Intro: responsive, image shadow, blockquote, hide image on mobile

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import './Intro.css';
 
 const Intro = () => (
-  <div className="Intro">
-    <blockquote className="Intro__summary">
-      <p>Dedicated and detail oriented, intelligent and ambitious. Adventurer thriving in versatile environments. I’m excited about modern web standards. Always improving.</p>
-    </blockquote>
-    <cite className="Intro__summary-author">Ole Frank Jensen - Senior Web Developer</cite>
-    <div className="Intro__portrait">
-      <img className="Intro__portrait-img" src="ole-frank-jensen-portrait.jpg" alt="Portrait of Ole Frank Jensen - Senior Web Developer" />
-    </div>
-  </div>
-)
+	<div className="Intro">
+		<blockquote className="Intro__summary">
+			<p>
+				Dedicated and detail oriented, intelligent and ambitious. Adventurer thriving in versatile environments. I’m
+				excited about modern web standards. Always improving.
+			</p>
+		</blockquote>
+		<cite className="Intro__author">Ole Frank Jensen - Senior Web Developer</cite>
+		<div className="Intro__portrait">
+			<div className="Intro__portrait--frame">
+				<img
+					className="Intro__portrait--img"
+					src="ole-frank-jensen-portrait.jpg"
+					alt="Portrait of Ole Frank Jensen - Senior Web Developer"
+				/>
+			</div>
+		</div>
+	</div>
+);
 export default Intro;
