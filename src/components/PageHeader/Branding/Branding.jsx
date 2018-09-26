01f86bfee3e9a329a21c5ecb501b18e6da657349
Ole <ole@Oles-MacBook-Pro.local>
2018-09-26 11:35:36 +0200

    PageHeader: responsive mobile, desktop, hide image, 'no-spacing' utils class

--- src/components/PageHeader/Branding/Branding.jsx
+++ src/components/PageHeader/Branding/Branding.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './Branding.css'
+import React from 'react';
+import './Branding.css';
 
 const PageHeaderBranding = () => (
-  <div className="PageHeaderBranding">
-    <img
-      className="PageHeaderBranding__photo"
-      src="ole-frank-jensen-portrait-square.jpg"
-      alt="Portrait of Ole Frank Jensen, Senior Web Developer"
-    />
-    <div className="PageHeaderBranding__name-title">
-      <h1>Ole Frank Jensen</h1>
-      <h2>Senior Web Developer</h2>
-    </div>
-  </div>
-)
-export default PageHeaderBranding
+	<div className="PageHeaderBranding">
+		<img
+			className="PageHeaderBranding__photo"
+			src="ole-frank-jensen-portrait-square.jpg"
+			alt="Portrait of Ole Frank Jensen, Senior Web Developer"
+		/>
+		<div className="PageHeaderBranding__name-title">
+			<h1 className="PageHeaderBranding__name ">Ole Frank Jensen</h1>
+			<h4 className="PageHeaderBranding__title">Senior Web Developer</h4>
+		</div>
+	</div>
+);
+export default PageHeaderBranding;
