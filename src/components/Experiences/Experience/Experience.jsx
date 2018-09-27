d9dd8539054c324e068daa09a8222fa9b93b8d02
Ole <ole@Oles-MacBook-Pro.local>
2018-09-27 15:12:53 +0200

    timeline visualization of  experience/education

--- src/components/Experiences/Experience/Experience.jsx
+++ src/components/Experiences/Experience/Experience.jsx
@@ -3,11 +3,11 @@ import Keywords from '../../Keywords/Keywords';
 import './Experience.css';
 
 const Experience = ({ data }) => (
-	<div className="Experience">
+	<li className="Experience event" data-date={data.dateFrom.getFullYear()}>
 		<h4 className="Experience__title">{data.title}</h4>
 		<h5 className="Experience__company">{data.company}</h5>
 		<p className="Experience__description">{data.description}</p>
 		<Keywords data={data.keywords} />
-	</div>
+	</li>
 );
 export default Experience;
