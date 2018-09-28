349693ebd29e3c50df1b5fdd4401bd5ebf3a2695
Ole <ole@Oles-MacBook-Pro.local>
2018-09-28 21:15:20 +0200

    centralize transition durations, various fine-tuning

--- src/components/PageHeader/Contact/Contact.jsx
+++ src/components/PageHeader/Contact/Contact.jsx
@@ -4,20 +4,14 @@ import './Contact.css';
 
 const PageHeaderContact = ({className}) => (
 	<div className={`PageHeaderContact ${className}`}>
-		<a href="https://www.facebook.com/ole.f.jensen.7" target="_blank" rel="noopener noreferrer">
-			<FontAwesomeIcon icon={['fab', 'facebook']} />
+		<a href="https://www.facebook.com/ole.f.jensen.7" target="_blank" rel="noopener noreferrer" className="PageHeaderContact__link">
+			<FontAwesomeIcon icon={['fab', 'facebook']} className="PageHeaderContact__icon" />
 		</a>
-		<a href="https://www.linkedin.com/in/olefrankjensen/" target="_blank" rel="noopener noreferrer">
-			<FontAwesomeIcon icon={['fab', 'linkedin']} />
+		<a href="https://www.linkedin.com/in/olefrankjensen/" target="_blank" rel="noopener noreferrer" className="PageHeaderContact__link">
+			<FontAwesomeIcon icon={['fab', 'linkedin']} className="PageHeaderContact__icon" />
 		</a>
-		<a href="https://github.com/olefrank" target="_blank" rel="noopener noreferrer">
-			<FontAwesomeIcon icon={['fab', 'github']} />
-		</a>
-		<a href="skype:olefrankjensen" target="_blank" rel="noopener noreferrer">
-			<FontAwesomeIcon icon={['fab', 'skype']} />
-		</a>
-		<a href="mailto:olefrankjensen@gmail.com" target="_blank" rel="noopener noreferrer">
-			<FontAwesomeIcon icon="envelope" />
+		<a href="https://github.com/olefrank" target="_blank" rel="noopener noreferrer" className="PageHeaderContact__link">
+			<FontAwesomeIcon icon={['fab', 'github']} className="PageHeaderContact__icon" />
 		</a>
 	</div>
 );
