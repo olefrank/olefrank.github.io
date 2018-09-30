4920e073d017b7d7e326febfb1f9ab02e3b7dea9
Ole <ole@Oles-MacBook-Pro.local>
2018-09-30 13:16:52 +0200

    Intro: add shor bio, styling tweaks

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,13 +3,16 @@ import './Intro.css';
 
 const Intro = () => (
 	<div className="Intro">
-		<blockquote className="Intro__summary">
-			<p>
-				Dedicated and detail oriented, intelligent and ambitious. Adventurer thriving in versatile environments. I’m
-				excited about modern web standards. Always improving.
-			</p>
+		<h5 className="Intro__byline">
+			7 years full time experience, AP Computer Science with top grades, experienced instructor/teacher, loves to hike,
+			kayak and play jazz trumpet, 40 years old, Copenhagen based.
+		</h5>
+
+		<blockquote className="Intro__quote">
+			<p>I am dedicated and detail oriented. Exited about modern web standards. Always learning, always improving...</p>
 		</blockquote>
 		<cite className="Intro__author">Ole Frank Jensen - Senior Web Developer</cite>
+
 		<div className="Intro__portrait">
 			<div className="Intro__portrait--frame">
 				<img
