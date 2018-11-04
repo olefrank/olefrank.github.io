085885c87f997a5003cb5a4fe5be02d3fe4696a8
Ole <olefrankjensen@gmail.com>
2018-11-04 16:15:10 +0100

    remove serviceworker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import 'sanitize.css';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
