107d4946b4405cba297967ae1f8cacc00927194a
Ole <olefrankjensen@gmail.com>
2018-11-04 16:27:52 +0100

    add contact link to my stack overflow profile page

--- src/components/PageHeader/Contact/Contact.jsx
+++ src/components/PageHeader/Contact/Contact.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Contact.css';
 
-const PageHeaderContact = ({className}) => (
+const PageHeaderContact = ({ className }) => (
 	<div className={`PageHeaderContact ${className}`}>
-		<a href="https://www.facebook.com/ole.f.jensen.7" target="_blank" rel="noopener noreferrer" className="PageHeaderContact__link">
+		<a
+			href="https://www.facebook.com/ole.f.jensen.7"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="PageHeaderContact__link">
 			<FontAwesomeIcon icon={['fab', 'facebook']} className="PageHeaderContact__icon" />
 		</a>
-		<a href="https://www.linkedin.com/in/olefrankjensen/" target="_blank" rel="noopener noreferrer" className="PageHeaderContact__link">
+		<a
+			href="https://www.linkedin.com/in/olefrankjensen/"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="PageHeaderContact__link">
 			<FontAwesomeIcon icon={['fab', 'linkedin']} className="PageHeaderContact__icon" />
 		</a>
 		<a href="https://github.com/olefrank" target="_blank" rel="noopener noreferrer" className="PageHeaderContact__link">
 			<FontAwesomeIcon icon={['fab', 'github']} className="PageHeaderContact__icon" />
+		</a>
+		<a
+			href="https://stackoverflow.com/users/1736012/olefrank?tab=profile"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="PageHeaderContact__link">
+			<FontAwesomeIcon icon={['fab', 'stack-overflow']} className="PageHeaderContact__icon" />
 		</a>
 	</div>
 );
